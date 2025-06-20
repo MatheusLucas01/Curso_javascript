06de55611de8c687e7a63e3a7eae200cdc717dcd
Matheus Lucas <matheuslucasdesouza22@gmail.com>
2025-06-20 10:57:16 -0300

    feat: nomes de variáves e snake_case

---
+++ aula 4 -Nomes de variáveis/scripts.js
@@ -0,0 +1,23 @@
+
+// Podemos, mas não recomendado
+let $email = "matheus@$email.com"
+let _email = "lucas@email.com"
+let Ação = Cadastrar
+
+/*
+Recomendações
+Não escrever em portugues
+Escrever nome de variável que faça sentido
+camelCase - Exemplo: userName ou 
+snake_case: 
+*/
+
+// snake_case
+let product_name = "Notebook"
+let user_name = "Matheus"
+
+
+//camelCase
+let productName = "Teclado"
+let userName = "Lucas"
+
