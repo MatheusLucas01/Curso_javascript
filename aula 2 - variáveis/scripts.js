1eaf184d1041bb04b37ba46a6b0ce5076b132546
Matheus Lucas <matheuslucasdesouza22@gmail.com>
2025-06-20 09:58:56 -0300

    feat: update comments for variable declaration clarity

--- aula 2 - variáveis/scripts.js
+++ aula 2 - variáveis/scripts.js
@@ -1,4 +1,4 @@
-// Como declarar uma variável sem valor
+// Como declarar uma variável sem valor, a variável var sobrescreve as outras, é mutável
 var user
 console.log(user)
 
@@ -8,3 +8,23 @@ var email = "matheus@email.com"
 // Substituir o valor da variável 
 email = "joao@email.com"
 console.log(email)
+
+var product = "Teclado"
+var Product = "Mouse"
+console.log(product, Product)
+
+// Javascript é case sensitive, ou seja, sensivel a letras maiusculas e minusculas
+
+// variávies let podem ser atualizadas, mas não declaradas novamente.
+let user
+console.log("Matheus Lucas")
+
+let user = "Matheus@email.com"
+console.log(email)
+
+email = "matheuslucas@email.com"
+console.log(email)
+
+//Variável com valor fixo, não pode ser alterado!!
+const number = 43
+console.log(number)
