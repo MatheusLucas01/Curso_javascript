786b25bb3bbc665ebf4bc8abb361803aa4fc8ee0
Matheus Lucas <matheuslucasdesouza22@gmail.com>
2025-06-20 09:41:16 -0300

    feat: Variáveis

---
+++ aula 2 - variáveis/README.md
@@ -0,0 +1,3 @@
+## Aula sobre variáveis
+
+Na programação, é um espaço reservado na memmória ram do computador para armazenar algo temporariamente.
---
+++ aula 2 - variáveis/scripts.js
@@ -0,0 +1,10 @@
+// Como declarar uma variável sem valor
+var user
+console.log(user)
+
+// Variável com valor
+var email = "matheus@email.com"
+
+// Substituir o valor da variável 
+email = "joao@email.com"
+console.log(email)
