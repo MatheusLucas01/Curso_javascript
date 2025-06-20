034b36000db0da9cb47be9ddfd1ad5a04626fa38
Matheus Lucas <matheuslucasdesouza22@gmail.com>
2025-06-20 11:35:31 -0300

    feat: Tipos de dados

---
+++ aula 5 - Tipos de dados/scripts.js
@@ -0,0 +1,30 @@
+// Tipos primitivos
+
+/*
+String - Guarda texto
+Number - Números
+Boolean - True or false
+null - Vazio
+undefined - Indefinido
+
+*/
+
+let user_name = "matheus"
+let email = "matheus@email.com"
+console.log(typeof user_name) // type0f --> Qual o tipo?
+
+// aspas duplas
+console.log("one string with quotation marks")
+
+// aspas duplas e aspas simples
+console.log('Para aparecer a aspas duplas "usa-se assim".')
+
+// Usando acento grave, permite escrever multiplas linhas
+console.log(`
+    Uma string com acento grave
+    permite escrever múltiplas linhas
+    `)
+
+// Template literals
+console.log(`Olá, ${user_name}. Você conectou com o E-mail: ${email}, seja bem vindo ao sistema!`)
+
