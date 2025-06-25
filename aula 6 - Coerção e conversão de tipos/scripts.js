f826c6386e42c87a941d7220abfc2f350a12f5af
Matheus Lucas <matheuslucasdesouza22@gmail.com>
2025-06-25 09:35:39 -0300

    feat: Coerção e conversão de tipos/incrementos

---
+++ aula 6 - Coerção e conversão de tipos/scripts.js
@@ -0,0 +1,31 @@
+// string
+let value = "9"
+
+// irá retornar string
+console.log(typeof value)
+
+// irá retornar number
+console.log(typeof Number(value))
+
+// Mudar para string com função
+let age = 18
+console.log(typeof age.toString())
+
+// Mudar para boolean
+let option = 1
+console.log(typeof Boolean(option))
+
+// Coerção de tipos
+console.log("10" + 5)
+
+
+// Incremento
+let number = 10
+// number++
+// console.log(number)
+console.log("Incremento Antes:", ++number)
+
+// Decremento
+let number1 = 20
+console.log("Decremento: ", --number1)
+
