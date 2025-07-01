c373fe0bc2094c6350aa4e104d8797b15f659cee
Matheus <matheuslucasdesouza22@gmail.com>
2025-07-01 17:55:54 -0300

    feat: Comentários com JSdoc

---
+++ aula 13 - Comentários com JsDoc/scripts.js
@@ -0,0 +1,18 @@
+/*
+    Comentário de documentação em JavaScript (sintaxe do JSdoc)
+    O JSdoc é um padrão para incorporar documentação no código fonte a partir desses comentários
+*/
+
+/**
+ * 
+ * @param {String} email user email.
+ * @param {String} password more than 6 characters.
+ * @returns {Number} user id.
+ */
+function signIn(email, password){
+    // Fluxo de autenticação do usuário
+
+    return 7
+}
+
+signIn("Matheus@email.com", "12345")
