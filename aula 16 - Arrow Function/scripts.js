f381203700c8a6bd7b9f5ac6e0647668f7000453
Matheus <matheuslucasdesouza22@gmail.com>
2025-07-02 17:55:08 -0300

    feat: Arrow Function - Funções de seta

---
+++ aula 16 - Arrow Function/scripts.js
@@ -0,0 +1,21 @@
+// Arrow function (função de seta)
+const showMessage = () => {
+    console.log("Oi")
+}
+
+showMessage()
+
+/*
+Usando arrow function + parametros + interpolação + argumentos ; 
+
+arrow function -> const funcao = () => {}
+parametros -> username, email --> Variaveis que recebem valor na função
+interpolação -> `${username} ${email}`
+argumentos -> "Matheus", "matheus@gmail.com" -> Valor que é passado na função.
+
+*/
+const showMessage2 = (username, email) => {
+   console.log(`Olá ${username}, seu email é: ${email}`) 
+}
+
+showMessage2("Matheus", "matheus@gmail.com")
